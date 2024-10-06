3118655392e63e2427a16830601ccd1fb5d224dd
shakil12 <tahmidul.work@gmail.com>
2024-10-06 20:56:01 +0600

    files are setup with change in cors policy

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const products = require('./products.json');
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: false
+}));
+
+
+app.get('/api/products', (req, res) => {
+  res.json(products); 
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === (req.params.id));
+  if (!product) return res.status(404).send('Product not found');
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
